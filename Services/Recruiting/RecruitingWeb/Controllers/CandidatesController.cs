e9899e547db212f58c10c1a2194e25b2a196adf2
Zhaobin Jin <jinlinyo@gmail.com>
2023-05-16 08:55:55 -0700

    Create project for day1

---
+++ Services/Recruiting/RecruitingWeb/Controllers/CandidatesController.cs
@@ -0,0 +1,13 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using RecruitingWeb.Models;
+
+namespace RecruitingWeb.Controllers;
+
+public class CandidatesController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
---
+++ Services/Recruiting/RecruitingWeb/Controllers/JobsController.cs
@@ -0,0 +1,30 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using RecruitingWeb.Models;
+
+namespace RecruitingWeb.Controllers;
+
+public class JobsController : Controller
+{
+    [HttpGet]
+    public IActionResult Index()
+    {
+        // all jobs can be applied
+        return View();
+    }
+    
+    [HttpGet]
+    public IActionResult Details(int id)
+    {
+        // get details
+        return View();
+    }
+    
+    [HttpPost]
+    public IActionResult Create()
+    {
+        // only authed and have role / HR ? manager
+        // take info from view then save to DB
+        return View();
+    }
+}
